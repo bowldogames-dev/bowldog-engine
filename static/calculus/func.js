c9f1a81ecd8a308520a2e01090715976e23141ff
Jordan <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2024-07-02 21:26:34 +0000

    m

--- static/calculus/func.js
+++ static/calculus/func.js
@@ -1,7 +1,8 @@
 const images = [
-    { id: 1, text: "Adventure Capatalist", action: "redirectUrl", url: "https://than1089.github.io/adventure-capitalist", imgSrc: "image1.jpg" },
-    { id: 2, text: "Retro Bowl", action: "redirectSrc", url: "/static/calculus/src/retro-bowl", imgSrc: "image2.jpg" },
-    { id: 3, text: "Image 3", action: "redirectUrl", url: "https://roblox.com", imgSrc: "image3.jpg" },
+    { id: 1, text: "Adventure Capatalist", action: "redirectUrl", url: "https://than1089.github.io/adventure-capitalist", imgSrc: "/img/sci/adventurecap.png" },
+    { id: 2, text: "Retro Bowl", action: "redirectSrc", src: "/static/calculus/source/retrobwl-unlimited", imgSrc: "/img/sci/rb.avif" },
+    { id: 3, text: "ROBL0X", action: "redirectUrl", url: "https://d886g6-8080.csb.app/", imgSrc: "/img/sci/rblx.png" },
+    { id: 4, text: "1", action: "redirectCdn", fileName: "1", imgSrc: "/img/sci/one.png" }
 ];
 
 const imageContainer = document.getElementById('image-container');
@@ -11,8 +12,9 @@ function displayImages(filteredImages) {
     imageContainer.innerHTML = '';
     filteredImages.forEach(image => {
         const imageItem = document.createElement('div');
+        imageItem.className = 'image-item';
         imageItem.innerHTML = `
-        <div class="image-item" onclick="handleAction(${image.id})">
+            <div class="image-item" onclick="handleAction(${image.id})">
             <img src="${image.imgSrc}" alt="${image.text}">
             <p>${image.text}</p>
             </div>
@@ -27,7 +29,9 @@ function handleAction(id) {
         if (image.action === 'redirectUrl') {
             redirectUrl(image.url);
         } else if (image.action === 'redirectSrc') {
-            redirectSrc(image.url);
+            redirectSrc(image.src, image.text);
+        } else if (image.action === 'redirectCdn') {
+            redirectCdn(image.fileName, image.text);
         }
     }
 }
@@ -36,8 +40,13 @@ function redirectUrl(url) {
     location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 }
 
-function redirectSrc(url) {
-    location.href = url;
+function redirectSrc(src, text) {
+    location.href = `/static/calculus/src.html?mylearningexperience=${encodeURIComponent(src)}&text=${encodeURIComponent(text)}`;
+}
+
+function redirectCdn(fileName, text) {
+    const url = `https://library.algebrafordummies.fun/GalacticNetwork/3kh0-assets/main/${fileName}/index.html`;
+    location.href = `/static/calculus/src.html?mylearningexperience=${encodeURIComponent(url)}&text=${encodeURIComponent(text)}`;
 }
 
 searchBar.addEventListener('input', () => {
