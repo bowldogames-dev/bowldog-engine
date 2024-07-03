2769f669431fd27c564b0bc73524ccac398ff393
Jordan <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2024-07-03 15:51:05 +0000

    yes

--- static/calculus/func.js
+++ static/calculus/func.js
@@ -1,36 +1,241 @@
-const images = [
-    { id: 1, text: "Adventure Capatalist", action: "redirectUrl", url: "https://than1089.github.io/adventure-capitalist", imgSrc: "/img/sci/adventurecap.png" },
-    { id: 2, text: "Retro Bowl", action: "redirectCdn", fileName: "retro-bowl", imgSrc: "/img/sci/rb.avif" },
-    { id: 3, text: "ROBL0X", action: "redirectUrl", url: "https://d886g6-8080.csb.app/", imgSrc: "/img/sci/rblx.png" },
-    { id: 4, text: "1", action: "redirectCdn", fileName: "1", imgSrc: "/img/sci/one.png" },
-    { id: 5, text: "1v1.LOL Old", action: "redirectSrc", src: "/static/calculus/source/1v1-lol/old", imgSrc: "/img/sci/1v1-lol.webp" },
-    { id: 6, text: "1v1.LOL NEW", action: "redirectSrc", src: "/static/calculus/source/1v1.lol/new", imgSrc: "/img/sci/1v1lol.jpg" },
-    { id: 7, text: "Achievement Unlocked", action: "redirectSrc", src: "/static/calculus/source/achievement-unlocked", imgSrc: "/img/sci/achievement.webp" },
-    { id: 8, text: "Achievement Unlocked 2", action: "redirectSrc", src: "/static/calculus/source/achievement-unlocked/2/", imgSrc: "/img/sci/achievement-2.png" },
-    { id: 9, text: "Achievement Unlocked 3", action: "redirectSrc", src: "/static/calculus/source/achievement-unlocked/3/", imgSrc: "/img/sci/achievement-3.webp" },
-    { id: 10, text: "Slope", action: "redirectSrc", src: "/static/calculus/source/slope", imgSrc: "/img/sci/slope.jpeg" },
-    { id: 11, text: "Eaglercraft 1.3.3 (old)", action: "redirectSrc", src: "/static/calculus/source/eaglercraft/133", imgSrc: "/img/sci/133.jpeg" },
-    { id: 12, text: "Eaglercraft 1.5.2 (local)", action: "redirectSrc", src: "/static/calculus/source/eaglercraft/152", imgSrc: "/img/sci/152.jpeg" },
-    { id: 13, text: "Eaglercraft 1.8.8 (server)", action: "redirectSrc", src: "/static/calculus/source/eaglercraft/188", imgSrc: "/img/sci/eaglercraft.jpg" },
-    { id: 14, text: "Eaglercraft 1.8.9", action: "redirectSrc", src: "/static/calculus/source/eaglercraft/189", imgSrc: "/img/sci/189.jpg" },
-    { id: 15, text: "Subway Surfers", action: "redirectSrc", src: "/static/calculus/source/subway-surfers", imgSrc: "/img/sci/subway-surfers.jpeg" },
-    { id: 16, text: "Basket Random", action: "redirectSrc", src: "/static/calculus/source/basket-random", imgSrc: "/img/sci/basket-random.jpeg" },
-    { id: 17, text: "Hanger 2", action: "redirectSrc", src: "/static/calculus/source/hanger-2", imgSrc: "/img/sci/hanger-2.jpg" },
-    { id: 18, text: "Tunnel Rush", action: "redirectSrc", src: "/static/calculus/source/tunnel-rush", imgSrc: "/img/sci/tunnel-rush.jpg" },
-    { id: 19, text: "Time Shooter 3", action: "redirectSrc", src: "/static/calculus/source/time-shooter-3", imgSrc: "/img/sci/ts3.jpg" },
-    { id: 20, text: "Totally Accurate Battle Sim", action: "redirectSrc", src: "/static/calculus/source/tabs", imgSrc: "/img/sci/tabs.avif" },
-    { id: 21, text: "Football Legends", action: "redirectSrc", src: "/static/calculus/source/football-legends", imgSrc: "/img/sci/fblegends.avif" },
-    { id: 22, text: "Paper.IO 2", action: "redirectSrc", src: "/static/calculus/source/paper-io2", imgSrc: "/img/sci/paperio2.jpeg" },
-    { id: 23, text: "Helix Jump", action: "redirectSrc", src: "/static/calculus/source/helix-jump", imgSrc: "/img/sci/helix-jump.jpeg" },
-    { id: 24, text: "A Small World Cup", action: "redirectSrc", src: "/static/calculus/source/a-small-world-cup", imgSrc: "/img/sci/aswc.avif" },
-    { id: 25, text: "Basketball Legends", action: "redirectSrc", src: "/static/calculus/source/basketball-legends", imgSrc: "/img/sci/basketball-legends.jpg" },
-    { id: 26, text: "JustFall.LOL", action: "redirectSrc", src: "/static/calculus/source/just-fall", imgSrc: "/img/sci/just-fall-lol.png" },
-    { id: 27, text: "Death Run 3D", action: "redirectSrc", src: "/static/calculus/source/death-run-3d", imgSrc: "/img/sci/death-run-3d.png" },
-    { id: 28, text: "CraftMine", action: "redirectSrc", src: "/static/calculus/source/craftmine", imgSrc: "/img/sci/craftmine.jpg" },
-    { id: 29, text: "Happy Wheels", action: "redirectSrc", src: "/static/calculus/source/happy-wheels", imgSrc: "/img/sci/happy-wheels.webp" },
-    { id: 30, text: "OvO", action: "redirectSrc", src: "/static/calculus/source/ovo", imgSrc: "/img/sci/ovo.png" },
-    { id: 31, text: "Soccer Random", action: "redirectSrc", src: "/static/calculus/source/soccer-random", imgSrc: "/img/sci/soccer-random.png" },
-    { id: 32, text: "Slope Tunnel", action: "redirectSrc", src: "/static/calculus/source/slope-tunnel", imgSrc: "/img/sci/slope-tunnel.jpg" }
+const images = [{
+        id: 1,
+        text: "Adventure Capatalist",
+        action: "redirectUrl",
+        url: "https://than1089.github.io/adventure-capitalist",
+        imgSrc: "/img/sci/adventurecap.png"
+    },
+    {
+        id: 2,
+        text: "Retro Bowl",
+        action: "redirectCdn",
+        fileName: "retro-bowl",
+        imgSrc: "/img/sci/rb.avif"
+    },
+    {
+        id: 3,
+        text: "ROBL0X",
+        action: "redirectUrl",
+        url: "https://d886g6-8080.csb.app/",
+        imgSrc: "/img/sci/rblx.png"
+    },
+    {
+        id: 4,
+        text: "1",
+        action: "redirectCdn",
+        fileName: "1",
+        imgSrc: "/img/sci/one.png"
+    },
+    {
+        id: 5,
+        text: "1v1.LOL Old",
+        action: "redirectSrc",
+        src: "/static/calculus/source/1v1-lol/old",
+        imgSrc: "/img/sci/1v1-lol.webp"
+    },
+    {
+        id: 6,
+        text: "1v1.LOL NEW",
+        action: "redirectSrc",
+        src: "/static/calculus/source/1v1.lol/new",
+        imgSrc: "/img/sci/1v1lol.jpg"
+    },
+    {
+        id: 7,
+        text: "Achievement Unlocked",
+        action: "redirectSrc",
+        src: "/static/calculus/source/achievement-unlocked",
+        imgSrc: "/img/sci/achievement.webp"
+    },
+    {
+        id: 8,
+        text: "Achievement Unlocked 2",
+        action: "redirectSrc",
+        src: "/static/calculus/source/achievement-unlocked/2/",
+        imgSrc: "/img/sci/achievement-2.png"
+    },
+    {
+        id: 9,
+        text: "Achievement Unlocked 3",
+        action: "redirectSrc",
+        src: "/static/calculus/source/achievement-unlocked/3/",
+        imgSrc: "/img/sci/achievement-3.webp"
+    },
+    {
+        id: 10,
+        text: "Slope",
+        action: "redirectSrc",
+        src: "/static/calculus/source/slope",
+        imgSrc: "/img/sci/slope.jpeg"
+    },
+    {
+        id: 11,
+        text: "Eaglercraft 1.3.3 (old)",
+        action: "redirectSrc",
+        src: "/static/calculus/source/eaglercraft/133",
+        imgSrc: "/img/sci/133.jpeg"
+    },
+    {
+        id: 12,
+        text: "Eaglercraft 1.5.2 (local)",
+        action: "redirectSrc",
+        src: "/static/calculus/source/eaglercraft/152",
+        imgSrc: "/img/sci/152.jpeg"
+    },
+    {
+        id: 13,
+        text: "Eaglercraft 1.8.8 (server)",
+        action: "redirectSrc",
+        src: "/static/calculus/source/eaglercraft/188",
+        imgSrc: "/img/sci/eaglercraft.jpg"
+    },
+    {
+        id: 14,
+        text: "Eaglercraft 1.8.9",
+        action: "redirectSrc",
+        src: "/static/calculus/source/eaglercraft/189",
+        imgSrc: "/img/sci/189.jpg"
+    },
+    {
+        id: 15,
+        text: "Subway Surfers",
+        action: "redirectSrc",
+        src: "/static/calculus/source/subway-surfers",
+        imgSrc: "/img/sci/subway-surfers.jpeg"
+    },
+    {
+        id: 16,
+        text: "Basket Random",
+        action: "redirectSrc",
+        src: "/static/calculus/source/basket-random",
+        imgSrc: "/img/sci/basket-random.jpeg"
+    },
+    {
+        id: 17,
+        text: "Hanger 2",
+        action: "redirectSrc",
+        src: "/static/calculus/source/hanger-2",
+        imgSrc: "/img/sci/hanger-2.jpg"
+    },
+    {
+        id: 18,
+        text: "Tunnel Rush",
+        action: "redirectSrc",
+        src: "/static/calculus/source/tunnel-rush",
+        imgSrc: "/img/sci/tunnel-rush.jpg"
+    },
+    {
+        id: 19,
+        text: "Time Shooter 3",
+        action: "redirectSrc",
+        src: "/static/calculus/source/time-shooter-3",
+        imgSrc: "/img/sci/ts3.jpg"
+    },
+    {
+        id: 20,
+        text: "Totally Accurate Battle Sim",
+        action: "redirectSrc",
+        src: "/static/calculus/source/tabs",
+        imgSrc: "/img/sci/tabs.avif"
+    },
+    {
+        id: 21,
+        text: "Football Legends",
+        action: "redirectSrc",
+        src: "/static/calculus/source/football-legends",
+        imgSrc: "/img/sci/fblegends.avif"
+    },
+    {
+        id: 22,
+        text: "Paper.IO 2",
+        action: "redirectSrc",
+        src: "/static/calculus/source/paper-io2",
+        imgSrc: "/img/sci/paperio2.jpeg"
+    },
+    {
+        id: 23,
+        text: "Helix Jump",
+        action: "redirectSrc",
+        src: "/static/calculus/source/helix-jump",
+        imgSrc: "/img/sci/helix-jump.jpeg"
+    },
+    {
+        id: 24,
+        text: "A Small World Cup",
+        action: "redirectSrc",
+        src: "/static/calculus/source/a-small-world-cup",
+        imgSrc: "/img/sci/aswc.avif"
+    },
+    {
+        id: 25,
+        text: "Basketball Legends",
+        action: "redirectSrc",
+        src: "/static/calculus/source/basketball-legends",
+        imgSrc: "/img/sci/basketball-legends.jpg"
+    },
+    {
+        id: 26,
+        text: "JustFall.LOL",
+        action: "redirectSrc",
+        src: "/static/calculus/source/just-fall",
+        imgSrc: "/img/sci/just-fall-lol.png"
+    },
+    {
+        id: 27,
+        text: "Death Run 3D",
+        action: "redirectSrc",
+        src: "/static/calculus/source/death-run-3d",
+        imgSrc: "/img/sci/death-run-3d.png"
+    },
+    {
+        id: 28,
+        text: "CraftMine",
+        action: "redirectSrc",
+        src: "/static/calculus/source/craftmine",
+        imgSrc: "/img/sci/craftmine.jpg"
+    },
+    {
+        id: 29,
+        text: "Happy Wheels",
+        action: "redirectSrc",
+        src: "/static/calculus/source/happy-wheels",
+        imgSrc: "/img/sci/happy-wheels.webp"
+    },
+    {
+        id: 30,
+        text: "OvO",
+        action: "redirectSrc",
+        src: "/static/calculus/source/ovo",
+        imgSrc: "/img/sci/ovo.png"
+    },
+    {
+        id: 31,
+        text: "Soccer Random",
+        action: "redirectSrc",
+        src: "/static/calculus/source/soccer-random",
+        imgSrc: "/img/sci/soccer-random.png"
+    },
+    {
+        id: 32,
+        text: "Slope Tunnel",
+        action: "redirectSrc",
+        src: "/static/calculus/source/slope-tunnel",
+        imgSrc: "/img/sci/slope-tunnel.jpg"
+    },
+    {
+        id: 33,
+        text: "Euro Cup",
+        action: "redirectSrc",
+        src: "/static/calculus/source/euro-cup",
+        imgSrc: "/img/sci/euro-cup.avif"
+    },
+    {
+        id: 34,
+        text: "Papa's Cupcakeria",
+        action: "redirectSrc",
+        src: "/static/calculus/source/cupcakeria",
+        imgSrc: "/img/sci/cupcakeria.png"
+    }
 ];
 
 const imageContainer = document.getElementById('image-container');
--- static/diameter/no.js
+++ static/diameter/no.js
@@ -1,20 +1,105 @@
-const images = [
-    { id: 1, text: "Invidious (youtube alt)", url: "https://invidious.drgns.space", imgSrc: "/img/edu/iv.png" },
-    { id: 2, text: "Google", url: "https://google.com", imgSrc: "/img/edu/google.jpg" },
-    { id: 3, text: "Nunflix", url: "https://nunflix.com", imgSrc: "/img/edu/nunflix.png" },
-    { id: 4, text: "YouTube Music", url: "https://music.youtube.com", imgSrc: "/img/edu/ytm.avif" },
-    { id: 5, text: "Cool Math Games", url: "https://coolmathgames.com", imgSrc: "/img/edu/mathgames.png" },
-    { id: 6, text: "Geforce Now", url: "https://play.geforcenow.com", imgSrc: "/img/edu/gfn.jpeg" },
-    { id: 7, text: "Twitch", url: "https://twitch.tv", imgSrc: "/img/edu/twitch.jpeg" },
-    { id: 8, text: "Reddit", url: "https://reddit.com", imgSrc: "/img/edu/reddit.png" },
-    { id: 9, text: "TikTok", url: "https://tiktok.com", imgSrc: "/img/edu/tiktok.png" },
-    { id: 10, text: "Aptoide", url: "https://d886g6-8080.csb.app/apps/aptoide/5874/aptoide-2.html", imgSrc: "/img/edu/aptoide.webp" },
-    { id: 11, text: "Amazon Luna", url: "https://luna.amazon.com", imgSrc: "/img/edu/amazon-luna.png" },
-    { id: 12, text: "Character.AI", url: "https://c.ai", imgSrc: "/img/edu/cai.png" },
-    { id: 13, text: "123Movies", url: "https://0123movie.net", imgSrc: "/img/edu/123movies.png" },
-    { id: 14, text: "Movie Web", url: "https://sudo-flix.lol", imgSrc: "/img/edu/mw.png" },
-    { id: 15, text: "Poki", url: "https://poki.com", imgSrc: "/img/edu/poki.png" },
-    { id: 16, text: "VS COde", url: "https://vscode.dev", imgSrc: "/img/edu/vsc.png" }
+const images = [{
+        id: 1,
+        text: "Invidious (youtube alt)",
+        url: "https://invidious.drgns.space",
+        imgSrc: "/img/edu/iv.png"
+    },
+    {
+        id: 2,
+        text: "Google",
+        url: "https://google.com",
+        imgSrc: "/img/edu/google.jpg"
+    },
+    {
+        id: 3,
+        text: "Nunflix",
+        url: "https://nunflix.com",
+        imgSrc: "/img/edu/nunflix.png"
+    },
+    {
+        id: 4,
+        text: "YouTube Music",
+        url: "https://music.youtube.com",
+        imgSrc: "/img/edu/ytm.avif"
+    },
+    {
+        id: 5,
+        text: "Cool Math Games",
+        url: "https://coolmathgames.com",
+        imgSrc: "/img/edu/mathgames.png"
+    },
+    {
+        id: 6,
+        text: "Geforce Now",
+        url: "https://play.geforcenow.com",
+        imgSrc: "/img/edu/gfn.jpeg"
+    },
+    {
+        id: 7,
+        text: "Twitch",
+        url: "https://twitch.tv",
+        imgSrc: "/img/edu/twitch.jpeg"
+    },
+    {
+        id: 8,
+        text: "Reddit",
+        url: "https://reddit.com",
+        imgSrc: "/img/edu/reddit.png"
+    },
+    {
+        id: 9,
+        text: "TikTok",
+        url: "https://tiktok.com",
+        imgSrc: "/img/edu/tiktok.png"
+    },
+    {
+        id: 10,
+        text: "Aptoide",
+        url: "https://d886g6-8080.csb.app/apps/aptoide/5874/aptoide-2.html",
+        imgSrc: "/img/edu/aptoide.webp"
+    },
+    {
+        id: 11,
+        text: "Amazon Luna",
+        url: "https://luna.amazon.com",
+        imgSrc: "/img/edu/amazon-luna.png"
+    },
+    {
+        id: 12,
+        text: "Character.AI",
+        url: "https://c.ai",
+        imgSrc: "/img/edu/cai.png"
+    },
+    {
+        id: 13,
+        text: "123Movies",
+        url: "https://0123movie.net",
+        imgSrc: "/img/edu/123movies.png"
+    },
+    {
+        id: 14,
+        text: "Movie Web",
+        url: "https://sudo-flix.lol",
+        imgSrc: "/img/edu/mw.png"
+    },
+    {
+        id: 15,
+        text: "Poki",
+        url: "https://poki.com",
+        imgSrc: "/img/edu/poki.png"
+    },
+    {
+        id: 16,
+        text: "VS Code",
+        url: "https://vscode.dev",
+        imgSrc: "/img/edu/vsc.png"
+    },
+    {
+        id: 17,
+        text: "SoundCloud",
+        url: "https://soundcloud.com",
+        imgSrc: "/img/edu/soundcloud.png"
+    }
 ];
 
 const imageContainer = document.getElementById('image-container');
