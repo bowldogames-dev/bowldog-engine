c2c49bc63f80d6135c1a07f828e9f7608288653e
Jordan <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2024-10-28 19:54:38 -0700

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/static/educational/',
+    prefix: '/static/jmwsw/',
     bare: 'https://abyss-web-intel.vercel.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
