b81f00a5f6726c8dcbb381bcd3a69dda3e2f4d82
Jordan <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2024-11-20 19:13:19 -0800

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/bdgedu/',
-    bare: 'https://studyverse.samyog.com.np/bare/',
+    bare: 'https://parallel-daphene-ijuslostmydooohg-021a963b.koyeb.app/fq/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
